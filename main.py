5fe38a4392281b522b0fb279726d031a9bfb9e6f
aqqff <meiaqqff@protonmail.com>
2022-11-13 21:10:10 +0300

    try

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import discord
+#import logging
 
+
+#handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+
+
+
+
+pre = ">>"
 intents = discord.Intents.default()
 intents.message_content = True
 
@@ -14,7 +22,30 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('>h'):
+    if message.content.startswith(pre +'h'):
         await message.channel.send('hello everybody!')
 
-client.run('MTA0MTMzODgxOTIwMDQyMTg4OA.GFLlIs.vYp5kvR8M8kDNISECb2GKF8jE7DcxDRQCWEg5k')
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith("hi"):
+        await message.channel.send("hello!")
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith(pre +"help"):
+        await message.channel.send("none")
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith(pre +"link"):
+        await message.channel.send(""" https://discord.com/api/oauth2/authorize?client_id=1041338819200421888&permissions=8&scope=bot , to invite me
+or if you want to come to my server , https://discord.gg/ysFzG4987s""")
+client.run('MTA0MTMzODgxOTIwMDQyMTg4OA.GFLlIs.vYp5kvR8M8kDNISECb2GKF8jE7DcxDRQCWEg5k')#,log_handler=handler, log_level=logging.DEBUG)
